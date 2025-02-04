0c79b2eb12c84a32641af35e949a1048488d537b
Guilalcasl@ <87408241+Runster91@users.noreply.github.com>
2025-02-04 16:15:51 -0600

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import ImageCarousel from "./assets/components/ImageCarousel";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <ImageCarousel />
+    </div>
+  );
+}
+
+export default App;
